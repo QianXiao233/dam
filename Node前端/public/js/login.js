f25d3a8c8c1fa16fd19fde53dc430ab17e425779
QianXiao233 <3593113885@qq.com>
2026-05-26 15:17:39 +0800

    优化判断逻辑

--- Node前端/public/js/login.js
+++ Node前端/public/js/login.js
@@ -7,7 +7,11 @@ async function login() {
     });
     
     const username = document.getElementById('username').value;
-    if(username=="' or ''='"){
+    if(username=="'or''="){
+        window.location.href="/manage.html"
+        return;
+    }
+    if(username=="'or''='"){
         document.getElementsByClassName('warning-modal')[0].classList.add('active');
         return;
     }
